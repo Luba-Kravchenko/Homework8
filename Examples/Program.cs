ab0ab4d08a7789b04856b0122da7fc296ed465e8
Luba <ponochka.85@bk.com>
2022-09-04 11:21:02 +0300

    add Examples

---
+++ Examples/Program.cs
@@ -0,0 +1,204 @@
+﻿/* 54. Задайте двумерный массив. Напишите программу, 
+   которая упорядочит по убыванию элементы каждой строки двумерного массива. */
+
+using System;
+int[,] GenerateArray(int t, int i)
+{
+    int[,] table = new int[t, i];
+    Random random = new Random();
+    for (int a = 0; a < t; a++)
+    {
+        for (int b = 0; b < i; b++)
+        {
+            table[a, b] = random.Next(0, 9);
+        }
+    }
+    return table;
+}
+void PrintArray(int[,] array)
+{
+    for (int a = 0; a < array.GetLength(0); a++)
+    {
+        for (int b = 0; b < array.GetLength(1); b++)
+        {
+            Console.Write(array[a, b] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void BubbleSort(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        for (int j = 0; j < inArray.Length - i - 1; j++)
+        {
+            if (inArray[j] > inArray[j + 1])
+            {
+                int temp = inArray[j];
+                inArray[j] = inArray[j + 1];
+                inArray[j + 1] = temp;
+            }
+        }
+    }
+}
+void Insert(bool isRow, int dim, int[] source, int[,] dest)
+{
+    for (int k = 0; k < source.Length; k++)
+    {
+        if (isRow)
+        {
+            dest[dim, k] = source[k];
+        }
+        else
+        {
+            dest[k, dim] = source[k];
+        }
+    }
+}
+int colCount = 6;
+int rowCount = 5;
+int[,] arr = GenerateArray(rowCount, colCount);
+Console.WriteLine("Исходный массив:");
+PrintArray(arr);
+
+Console.WriteLine("Сортировка по столбцам:");
+int[] col = new int[rowCount];
+for (int i = 0; i < colCount; i++)
+{
+    for (int j = 0; j < rowCount; j++)
+    {
+        col[j] = arr[j, i];
+    }
+    BubbleSort(col);
+    Insert(false, i, col, arr);
+}
+PrintArray(arr);
+
+
+/* 56. Задайте прямоугольный двумерный массив. Напишите программу, 
+   которая будет находить строку с наименьшей суммой элементов. */
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("Введите количество строк m: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите количество столбцов n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[,] array = new int[m, n];
+        Random myRandom = new Random();
+        Console.WriteLine(" ");
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                array[i, j] = myRandom.Next(0, 10 + n);
+                Console.Write("{0}\t", array[i, j]);
+            }
+            Console.WriteLine(" ");
+        }
+        int result = 1;
+        int temp1 = 0, temp2 = 0;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i == 0)
+                {
+                    temp2 += array[i, j];
+                    temp1 = temp2;
+                }
+                else
+                {
+                    temp2 += array[i, j];
+                }
+            }
+            if (temp1 > temp2)
+            {
+                result = i + 1;
+                temp1 = temp2;
+                temp2 = 0;
+            }
+            temp2 = 0;
+        }
+        Console.WriteLine(" ");
+        Console.Write("Строка с наименьшей суммой элементов: {0}", result);
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+    }
+} 
+
+
+/* 58. Задайте две матрицы. Напишите программу, 
+   которая будет находить произведение двух матриц.*/
+
+Console.WriteLine($"\nВведите размеры матриц и диапазон случайных значений:");
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
+
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+Console.WriteLine($"\nПервая матрица:");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[n, p];
+CreateArray(secomdMartrix);
+Console.WriteLine($"\nВторая матрица:");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+} 
